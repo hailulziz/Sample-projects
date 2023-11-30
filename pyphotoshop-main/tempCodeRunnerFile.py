690d7df54d8e7c7fce9409927c44409dafdfdff3
muth23102024 <muthonihannahhailu@gmail.com>
2023-11-30 08:44:17 +0300

    Final changes to pyphotoshop-main: Return after you learn DS

---
+++ pyphotoshop-main/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+# blur using kernel 3
+    blur_3 = blur(city, 3)
+    blur_3.write_image('blur_k3.png')
--- pyphotoshop-main/transform.py
+++ pyphotoshop-main/transform.py
@@ -40,6 +40,7 @@ def blur(image, kernel_size):
     # kernel size should always be an *odd* number
     x_pixels, y_pixels, num_channels = image.array.shape  # represents x, y pixels of image, # channels (R, G, B)
     new_im = Image(x_pixels=x_pixels, y_pixels=y_pixels, num_channels=num_channels)  # making a new array to copy values to!
+    print(x_pixels, y_pixels, num_channels)
     neighbor_range = kernel_size // 2  # this is a variable that tells us how many neighbors we actually look at (ie for a kernel of 3, this value should be 1)
     for x in range(x_pixels):
         for y in range(y_pixels):
@@ -107,9 +108,9 @@ if __name__ == '__main__':
     # decr_contrast = adjust_contrast(lake, 0.5, 0.5)
     # decr_contrast.write_image('decreased_contrast.png')
 
-    # # blur using kernel 3
-    # blur_3 = blur(city, 3)
-    # blur_3.write_image('blur_k3.png')
+    # blur using kernel 3
+    blur_3 = blur(city, 3)
+    blur_3.write_image('blur_k3.png')
 
     # # blur using kernel size of 15
     # blur_15 = blur(city, 15)
